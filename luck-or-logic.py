a7bec773f35256e22a5f56d2d8a0c50b6965b0d7
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2016-12-27 14:11:59 +0800

    Created basic backbone for program
    I've created the basic backbone of the program, with the operation selection, exiting, and if an operation isn't recognised. Also, as you can see, I'm going to start work on the first game, blackjack.

--- luck-or-logic.py
+++ luck-or-logic.py
@@ -1,1 +1,19 @@
+import sys
+games = ["blackjack"]
+startup = True
+while True:
+	if startup == True:
+		game = input("Welcome to Luck or Logic. Please choose a game to play. Type games to see what games there are available. Type exit to exit. \n")
+		startup = False
+	else:
+		game = input("What do you want to do? \n")
+	game = game.lower()
+	if game == "blackjack":
+		pass
+	elif game == "games":
+		print (games)
+	elif game == "exit":
+		sys.exit()
+	else:
+		print ("That's not a valid operation")
 
